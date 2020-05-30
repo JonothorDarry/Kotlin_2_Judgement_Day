90bb52faeea8855b2985da65db8e887c4161190b
Sebastian Michoń <sebastian.michon10@protonmail.com>
2020-05-30 17:16:20 +0200

    Less ridiculous table view

--- app/src/main/java/com/example/bricklist/SingleSet.kt
+++ app/src/main/java/com/example/bricklist/SingleSet.kt
@@ -3,12 +3,13 @@ package com.example.bricklist
 import android.content.Intent
 import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.util.Log
+import android.view.Gravity
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.LinearLayout
-import android.widget.TextView
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.graphics.scale
+import androidx.core.view.marginLeft
 
 
 class SingleSet : AppCompatActivity() {
@@ -26,14 +27,17 @@ class SingleSet : AppCompatActivity() {
             var color_id: TextView
             var plusButt: Button
             var minusButt: Button
+            var wrapperLayout: TableRow
+            var buttons: TableRow
             var image: ImageView
             var barray: ByteArray?
             var plname: String?
+            var wisdom: LinearLayout
 
             var vk : LinearLayout
 
             for (x in lst) {
-                vk =LinearLayout(this)
+                vk =TableLayout(this)
                 vk.layoutParams=ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
                 vk.orientation=LinearLayout.VERTICAL
 
@@ -43,6 +47,13 @@ class SingleSet : AppCompatActivity() {
                 minusButt= Button(this)
                 plusButt= Button(this)
                 image= ImageView(this)
+                buttons= TableRow(this)
+                wisdom= LinearLayout(this)
+                wrapperLayout=TableRow(this)
+
+                wisdom.orientation=LinearLayout.VERTICAL
+                buttons.orientation=LinearLayout.VERTICAL
+                wrapperLayout.orientation=LinearLayout.VERTICAL
 
                 minusButt.text="-"
                 plusButt.text="+"
@@ -57,12 +68,17 @@ class SingleSet : AppCompatActivity() {
                     enumera.text=x.QuantityInStore.toString()+" of "+x.QuantityInSet.toString()
                     barray=Databaze.myDb?.getMyrDao()?.getImage(x.ColorID, x.ItemID)
                     if (barray!=null){
-                        image.setImageBitmap(BitmapFactory.decodeByteArray(barray, 0, barray.size))
+                        image.setImageBitmap(BitmapFactory.decodeByteArray(barray, 0, barray.size).scale(300, 300))
                     }
                 }
+                image.maxWidth=540
+                image.minimumWidth=540
+                name.maxWidth=540
+                name.minWidth=540
 
-                minusButt.layoutParams=ViewGroup.LayoutParams(120, ViewGroup.LayoutParams.WRAP_CONTENT)
-                plusButt.layoutParams=ViewGroup.LayoutParams(120, ViewGroup.LayoutParams.WRAP_CONTENT)
+
+                buttons.layoutParams=ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+                vk.layoutParams=ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
 
                 minusButt.setOnClickListener {
                     Buttonizer.change(-1, x, enumera)
@@ -72,12 +88,17 @@ class SingleSet : AppCompatActivity() {
                     Buttonizer.change(1, x, enumera)
                 }
 
-                vk.addView(name)
-                vk.addView(color_id)
-                vk.addView(enumera)
-                vk.addView(minusButt)
-                vk.addView(plusButt)
-                vk.addView(image)
+                wisdom.addView(name)
+                wisdom.addView(color_id)
+                wisdom.addView(enumera)
+                wrapperLayout.addView(image)
+                wrapperLayout.addView(wisdom)
+
+                buttons.addView(plusButt)
+                buttons.addView(minusButt)
+
+                vk.addView(wrapperLayout)
+                vk.addView(buttons)
 
                 allList.addView(vk)
             }
@@ -121,4 +142,8 @@ view.layoutParams = param2*/
 val param = v2.layoutParams as ViewGroup.MarginLayoutParams
 param.setMargins(0,100,100,100)
 v2.layoutParams = param
+
+wrapperLayout.gravity=Gravity.LEFT
+                wrapperLayout.layoutParams=ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+                (wrapperLayout.layoutParams as LinearLayout.LayoutParams).weight= 0.5F
 */
